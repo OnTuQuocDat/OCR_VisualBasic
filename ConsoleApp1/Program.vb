9c9d99878a68dbc31a112974c5d8cdef69e5470d
OnTuQuocDat <quocdat.ot98@gmail.com>
2023-05-01 21:20:06 +0700

    first_2

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -2,16 +2,16 @@
 
 Module Program
     Dim lastLine As String '= "BB3ZR0D"
-    Dim result As Integer
+    Dim result_index As Integer
 
     Function find_index()
         ' Tim index cua ký tự chuỗi lastLine  đang nằm ở index thứ mấy trong file tổng --> sau đó lấy "result index" ra để cộng thêm 80 con tiếp theo
         Dim text() As String = System.IO.File.ReadAllLines("test_generate_serial.txt")
         'Dim Findstring = IO.File.ReadAllText("test_generate_serial.txt")
-        result = Array.FindIndex(text, Function(s) s = lastLine)
-        result = result + 1
-        If result >= 0 Then
-            Console.WriteLine(result)
+        result_index = Array.FindIndex(text, Function(s) s = lastLine)
+        result_index = result_index + 1
+        If result_index >= 0 Then
+            Console.WriteLine(result_index)
         Else
             Console.WriteLine("Cannot find the index for this serial in file")
         End If
@@ -22,9 +22,14 @@ Module Program
         Dim lines() As String = IO.File.ReadAllLines("temp.txt")
         lastLine = lines(lines.Length - 1)
         Console.WriteLine(lastLine)
-
     End Function
 
+    Function Generate_serial()
+        Dim generate() As String = IO.File.ReadAllLines("test_generate_serial.txt")
+        For i As Integer = result_index To generate.Length - 1
+            Console.WriteLine("i value: {0}", i)
+        Next
+    End Function
 
 
 
@@ -32,7 +37,7 @@ Module Program
         Console.WriteLine("Hello World!")
         Read_final_serial_from_txt()
         find_index()
-
+        Generate_serial()
     End Sub
 
 End Module
