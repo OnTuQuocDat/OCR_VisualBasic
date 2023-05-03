63ee46cec8b4c7abb05c79e934bb20d3a514a1fa
OnTuQuocDat <quocdat.ot98@gmail.com>
2023-05-03 13:11:50 +0700

    03-05

--- ConsoleApp1/Program.vb
+++ ConsoleApp1/Program.vb
@@ -1,8 +1,12 @@
 ﻿Imports System
 
 Module Program
+    Const Fixture_Rows = 8
+    Const Fixture_Column = 10
     Dim lastLine As String '= "BB3ZR0D"
     Dim result_index As Integer
+    Dim save_serial_fornext As String
+    Dim generate() As String = IO.File.ReadAllLines("test_generate_serial.txt")
 
     Function find_index()
         ' Tim index cua ký tự chuỗi lastLine  đang nằm ở index thứ mấy trong file tổng --> sau đó lấy "result index" ra để cộng thêm 80 con tiếp theo
@@ -25,11 +29,33 @@ Module Program
     End Function
 
     Function Generate_serial()
-        Dim generate() As String = IO.File.ReadAllLines("test_generate_serial.txt")
-        For i As Integer = result_index To generate.Length - 1
+        For i As Integer = result_index To result_index + 81
             Console.WriteLine("i value: {0}", i)
         Next
+        ''''''''''''''''''''''''''Note chỗ này vô nè
+        'If result_index < 81 Then
+        'result_index = result_index + 1
+        'End If
+        ''''''''''''''''''''
     End Function
+
+    Function Save_lastserial()
+        save_serial_fornext = generate(result_index + 80)
+        'save_serial_fornext = "6W3NV4W"
+
+        Dim file_save_name As String = "temp.txt"
+        Using writer As System.IO.StreamWriter = New System.IO.StreamWriter("temp.txt", 1)
+            writer.WriteLine(save_serial_fornext)
+
+        End Using
+        'Dim objWriter As New System.IO.StreamWriter(file_save_name)
+        'objWriter.Write(save_serial_fornext)
+        'objWriter.Close()
+
+        Console.WriteLine("Write done")
+
+    End Function
+
 
 
 
@@ -37,7 +63,12 @@ Module Program
         Console.WriteLine("Hello World!")
         Read_final_serial_from_txt()
         find_index()
+        'Start loop for i for j ngay đây
         Generate_serial()
+
+        'Save serial for next start
+        Save_lastserial()
+
     End Sub
 
 End Module
